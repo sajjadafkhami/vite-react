1b1d25541f32a1c66ea6015d4e7b99c77de682a5
sajjadafkhami <sajad.afkhami10@gmail.com>
2023-05-06 14:44:29 +0330

    feat:conditional & event

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,14 +1,35 @@
-function ListGroup() {
-  const items = ["Iran", "Jepan", "Brizil", "Indian", "France"];
+import React from "react";
+
+//import { MouseEvent } from "react";
+
+const ListGroup: React.FC = () => {
+  let items: any = ["Iran", "Jepan", "Brizil", "Indian", "France"];
+
+  // items[0] = 3;
+  // items[1] = " he the man ";
+  // const handlerclick = (event : mouseevent)  => console.log(event);
+  const getMessage = () => {
+    //return items.length === 0 && <p>No item Found</p>;
+    return items.length === 0 ? <p>No item Found</p> : null;
+  };
+
   return (
     <>
-      <h1>ListGroup</h1>
+      <h1>List</h1>
+      {getMessage()};
       <ul className="list-group">
         {items.map((item) => (
-          <li Key={item}>{item}</li>
+          <li
+            className="list-group-item"
+            Key={item}
+            //{handlerclick()}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
   );
-}
+};
 export default ListGroup;
